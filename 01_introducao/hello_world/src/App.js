0edafd4ae026ed59079307ce9c4cb815c5197b8f
Julian Andrade Silva <julian-andrade@hotmail.com>
2022-05-28 23:45:33 -0300

    Introduction commit

---
+++ 01_introducao/hello_world/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App-color">Hello World React!</h1>
+      <h2>Bem vindo ao curso de React</h2>
+    </div>
+  );
+}
+
+export default App;
